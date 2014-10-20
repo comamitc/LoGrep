029d9b8ce83e4c9576cd0c871e92445cda071fae
Mitch Comardo <mcomardo@pros.com>
2014-10-20 15:58:04 -0500

    recursion and wildcards working

--- src/main/scala/LoGrep.scala
+++ src/main/scala/LoGrep.scala
@@ -1,34 +1,51 @@
 import scala.util.matching.Regex
 import java.io.File
+import java.nio.file.Files
 import java.io.FileNotFoundException
 
 import io.TextFile
 
 object LoGrep {
 
+	val anyThing = ".*".r
   val newLine = "(^\\d{4}-\\d{2}-\\d{2}[ T]\\d{2}:\\d{2}:\\d{2}.*)".r
   val ignoreLine = "(^\\s*\\t*\\r*)".r
 
-  /* TODO: handle filepat wildcards
-	def filesWithWildcard(filePat: String) = {
-  	if (filePat.contains('*')) {
-
-  	} else new File(filePat)
-  }*/
-
+  def isFile(file: File, wildCard: String) =
+  	(file.isFile() && file.getName().matches(wildCard)/* && Files.probeContentType(file.toPath) == "text/plain"*/)
   /**
    * Converts a File or a Directory to a List[File]
    *
    * TODO: handle filter out non 'text/plain' formats
    */
-  def fileListAdapter(file: File): List[File] = {
+  def fileListAdapter(file: File, wildCard: String): List[File] = {
     if (file.exists()) {
-      if (file.isDirectory()) file.listFiles().toList
-      else if (file.isFile()) List(file)
-      else throw new Exception("System type unknown for: " + file.getPath())
+      if (file.isDirectory())
+        file.listFiles()
+          .foldLeft(Nil: List[File])((acc, f) => fileListAdapter(f, wildCard) ::: acc) // recursion by default
+      else if (isFile(file, wildCard)) List(file)
+      else Nil
     } else {
       throw new FileNotFoundException(file.getPath() + " not found")
     }
+  }
+
+  def assembleWildCard(wildCard: String): String =
+  	wildCard.split('*')
+  		.map(x => if (x.isEmpty) "(.*)" else "(" + x + ")")
+  		.mkString("(.*)")
+
+  def assemblePath(list: List[String]) =
+  	if (list.isEmpty || list.head.isEmpty) "./"
+  	else list.foldLeft("")((acc, s) => s + '/' + acc)
+
+  // TODO: handle filepat wildcards
+  def normalizeFilePattern(pat: String): List[File] = {
+  	if (pat.contains('*')) {
+  		val paths = pat.split(Array('\\', '/')).toList.reverse
+  		fileListAdapter(new File(assemblePath(paths.tail)),
+  			assembleWildCard(paths.head))
+  	} else fileListAdapter(new File(pat), "(.*)")
   }
 
   /**
@@ -44,21 +61,21 @@ object LoGrep {
    */
   def parseFile(term: Regex)(f: File): Unit = {
     val func = printMatchedString(term) _
-    new TextFile(f.getCanonicalPath, newLine, ignoreLine)
+    new TextFile(f.getAbsolutePath, newLine, ignoreLine)
       .getLines(func)
   }
 
   /**
    *
    */
-  def processFiles(term: Regex = ".*".r, filePat: String = "."): Unit = {
+  def processFiles(term: Regex = anyThing, filePat: String = "."): Unit = {
     val func = parseFile(term) _
     def inner(list: List[File]): Unit = list match {
       case head :: Nil => func(head)
-      case head :: tail => func(head);inner(tail);
-      case Nil => throw new Exception("Unknown state for processor.")
+      case head :: tail => func(head); inner(tail);
+      case _ => {} // do nothing on an empty list of files
     }
-    inner(fileListAdapter(new File(filePat)))
+    inner(normalizeFilePattern(filePat))
   }
 
   /**
