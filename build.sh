c4ed4ca1fda299596361f14898e2714a6abbaa4e
Mitch Comardo <comamitc@comamitc-mac.local>
2014-10-30 21:55:17 -0500

    adding README to build file

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 sbt assembly
-zip logrep.zip logrep.sh logrep.bat config/uniform.conf target/scala-2.11/logrep-assembly-0.1.jar
+zip logrep.zip logrep.sh logrep.bat README.md config/uniform.conf target/scala-2.11/logrep-assembly-0.1.jar
