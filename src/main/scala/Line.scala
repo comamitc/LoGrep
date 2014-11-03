84004a029884689c3af97fd9fc1ee1b6d0e28635
Mitch Comardo <mcomardo@pros.com>
2014-11-03 13:02:04 -0600

    adding date_format to configuration

--- src/main/scala/Line.scala
+++ src/main/scala/Line.scala
@@ -1,19 +1,22 @@
 import com.github.nscala_time.time.Imports.DateTime
 import com.github.nscala_time.time.Imports.DateTimeFormat
 
+import uniform.config.UniformConfig
+
 trait Line {
-	val date: DateTime
+  val date: DateTime
 }
 
 case object NoLine extends Line {
-	val date: DateTime = null
+  val date: DateTime = null
 }
 
 case class LogLine(line: String) extends Line {
 
-	val fromFormat = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss")
+  val fromFormat = DateTimeFormat
+    .forPattern(UniformConfig.get("date_format"))
 
-	def fromTomcat(t: String): DateTime = fromFormat.parseDateTime(t)
+  def fromTomcat(t: String): DateTime = fromFormat.parseDateTime(t)
 
-	val date = fromTomcat(line.slice(0, 19))
+  val date = fromTomcat(line.slice(0, 19))
 }
